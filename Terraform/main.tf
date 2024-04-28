448fd3b1d9f311978293a490d2bc5e0ee51e91d4
Szymon Sobczyk <ssobczyk7@gmail.com>
2024-04-28 23:01:24 +0200

    fixed: TF format

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -15,8 +15,8 @@ terraform {
 }
 
 provider "azurerm" {
-  use_oidc                   = true  
-  storage_use_azuread        = true
+  use_oidc            = true
+  storage_use_azuread = true
   features {
 
     application_insights {
