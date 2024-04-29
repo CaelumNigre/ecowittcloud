214e3e3e76a81cf0d5cf46a38dea0ef7bf02e7b5
CaelumNigre <ssobczyk7@gmail.com>
2024-04-29 23:36:14 +0200

    Fixed KV again (#11)
    * changed: added KV allow network policy
    
    * fixed: TF syntax

--- Terraform/KV/main.tf
+++ Terraform/KV/main.tf
@@ -17,4 +17,8 @@ resource "azurerm_key_vault" "keyvault" {
   soft_delete_retention_days = 7
   enable_rbac_authorization  = true
   purge_protection_enabled   = true
+  network_acls {
+    bypass         = "AzureServices"
+    default_action = "Allow"
+  }
 }
