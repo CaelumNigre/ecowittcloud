2c40afc5d5de137d7e4b4ce713e0b9b0e5694bc3
Szymon Sobczyk <ssobczyk7@gmail.com>
2024-12-10 21:51:50 +0100

    added: storage account app setting

--- Terraform/Azure Functions/main.tf
+++ Terraform/Azure Functions/main.tf
@@ -88,6 +88,7 @@ resource "azurerm_windows_function_app" "fapp" {
     WEBSITE_CONTENTSHARE                     = "${var.fapp_name}-${random_string.sharesuffix.result}"
     KV_NAME                                  = "${var.kv_name}"
     TENANT_ID                                = data.azurerm_client_config.current.tenant_id
+    CONFIG_STORAGE                           = azurerm_storage_account.fapp-data.name
     netFrameworkVersion                      = "v6.0"
   }
 
